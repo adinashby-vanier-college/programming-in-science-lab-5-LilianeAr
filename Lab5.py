8fba92e93123d04f68c1331835a73526099fb2d5
LilianeAr <xdbsvzj9kc@privaterelay.appleid.com>
2026-01-07 12:27:42 -0500

    Update Lab5.py
    Refactor hollow_square and number_pattern functions to correctly generate patterns. Implemented logic for printing hollow squares and number patterns.

--- Lab5.py
+++ Lab5.py
@@ -5,14 +5,41 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    hollow_squares = ["*" * n]
+    for hollow_squares in range(n):
+        print(hollow_squares)
+    for hollow_squares in range(n-3):
+        print(hollow_squares)
+    return "*" * n
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+
+    for i in range(1, n + 1): 
+        for j in range(1, i + 1):
+            # print(, end="")
+            result += str(j)
+        #print()
+        result += "\n"
+
+    return result.rstrip()
+
+print(number_pattern(4))
+        
+#     pattern = ""
+#     for i in range(1, n + 1):
+#         line = ""
+#         for number in range(1, i + 1):
+#             line += str(number)
+#         pattern += line + "\n"
+#     return pattern
+# print(pattern)
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
